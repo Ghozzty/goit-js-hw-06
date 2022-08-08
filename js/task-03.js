7a82345da82ea9ca9469433a54941b3447f4de13
Fedor <ghozztt@gmail.com>
2022-08-08 22:28:07 +0300

    work on hw_6

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,22 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const ulGallery = document.querySelector('.gallery');
+
+  
+const galleryItems = images
+.map(({url, alt}) => `<li><img src= ${url} alt= ${alt} width=300/></li>`
+)
+.join('');
+;
+
+ulGallery.insertAdjacentHTML('afterbegin', galleryItems);
+ulGallery.style.listStyle = 'none';
+ulGallery.style.display = 'flex';
+ulGallery.style.gap = '50px';
+
+console.log(galleryItems);
+console.log(ulGallery);
+
+// console.log(ulGallery)
